d8fe493318b84ce4c37a1607d59fbab42bca4579
Oleksandr Kosevych <rn.cruger@gmail.com>
2020-05-29 17:29:43 +0300

    back

--- packages/backoffice-amplify/amplify/backend/function/transactionsValidate/src/index.js
+++ packages/backoffice-amplify/amplify/backend/function/transactionsValidate/src/index.js
@@ -1,40 +1,34 @@
+
 console.log('Loading function');
 var AWS = require('aws-sdk');
+
+const { helpers } = require('./moduler');
 const { v4: uuidv4 } = require('uuid');
+// const AWSendpoint = 'http://docker.for.mac.localhost:8000';
 var docClient = new AWS.DynamoDB.DocumentClient();
-console.log('import helpers');
 
-const {helpers} = require('./moduler');
+exports.handler = async (event, context, callback) => {
+  helpers.reportMessage('Received event');
 
-exports.handler = async (event, context) => {
-  let response = {
-
-  };
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  const transaction = helpers.extractTransaction(event.body !== undefined ? JSON.parse(event.body) : event);
-  const customers = await helpers.lookupCustomers(docClient)(transaction);
-  if(Array.isArray(customers)){
-    const knownCustomer = helpers.matchCustomer(transaction, customers);
-    if(knownCustomer === transaction) {
-      transaction.id = uuidv4();
-      const customer = await helpers.createCustomer(docClient)(transaction);
-      return {
-        statusCode: 200,
-        body: JSON.stringify({KYC: customer})
-      }
+  try {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    const transaction = helpers.extractTransaction(event.body !== undefined ? JSON.parse(event.body) : event);
+    const lookupResult = await helpers.registerIfUnknown(docClient)(transaction, uuidv4());
+    if (lookupResult.code > 200) {
+      return lookupResult;
     }
-    if( knownCustomer !== null) {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(customers)
-      };
+    const isAllowed = helpers.kycCheck(lookupResult.result, transaction);
+    const message = helpers.successResponse('We are done here!');
+    return {
+      KYC: lookupResult.result.KYC_State,
+      Tier: lookupResult.result.Tier,
+      PaymentAllowed: isAllowed,
+      ...message
     }
+  } catch (error) {
+    helpers.reportMessage('Error processing request.');
+    console.log(helpers.buildTrace());
+    // TODO: response builder
+    return error;
   }
-  else {
-    response = {
-      statusCode: 500,
-      body: JSON.stringify(event)
-    }
-  }
-  return response;
 };
