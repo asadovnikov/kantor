1881b53ca500827a35ce10040effcd455f727d25
Oleksandr Kosevych <rn.cruger@gmail.com>
2020-06-02 02:17:32 +0300

    register funcio

--- packages/shop-app/amplify/backend/function/registertransaction/src/app.js
+++ packages/shop-app/amplify/backend/function/registertransaction/src/app.js
@@ -25,71 +25,23 @@ app.use(bodyParser.json())
 app.use(awsServerlessExpressMiddleware.eventContext())
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
-});
-
-
-/**********************
- * Example get method *
- **********************/
-
-app.get('/transaction/register', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
-});
-
-app.get('/transaction/register/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'get call succeed!', url: req.url});
 });
 
 /****************************
 * Example post method *
 ****************************/
 
-app.post('/transaction/register', function(req, res) {
+app.post('/transaction/register', function (req, res) {
   // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+  res.json({ success: 'post call succeed!', url: req.url, body: req.body })
 });
 
-app.post('/transaction/register/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'post call succeed!', url: req.url, body: req.body})
-});
-
-/****************************
-* Example put method *
-****************************/
-
-app.put('/transaction/register', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-app.put('/transaction/register/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
-});
-
-/****************************
-* Example delete method *
-****************************/
-
-app.delete('/transaction/register', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.delete('/transaction/register/*', function(req, res) {
-  // Add your code here
-  res.json({success: 'delete call succeed!', url: req.url});
-});
-
-app.listen(3000, function() {
-    console.log("App started")
+app.listen(3000, function () {
+  console.log("App started")
 });
 
 // Export the app object. When executing the application local this does nothing. However,
--- packages/shop-app/amplify/backend/function/registertransaction/src/index.js
+++ packages/shop-app/amplify/backend/function/registertransaction/src/index.js
@@ -1,9 +1,46 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
+console.log('Loading function');
+var AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+var docClient = new AWS.DynamoDB.DocumentClient();
+const tblName = process.env.API_SHOPAPPAPI_TRANSACTIONTABLE_NAME;
 
-const server = awsServerlessExpress.createServer(app);
+const extractTransaction = (message) => {
+  reportMessage('Extracting transaction from a message');
+  if (isTransactionRequest(message)) {
+    return prepareTrRequest(message);
+  }
+  if (message.transaction !== undefined) {
+    if (isTransactionRequest(message.transaction)) {
+      return prepareTrRequest(message.transaction);
+    }
+  }
+  if (message.body !== undefined) {
+    if (isTransactionRequest(message.body)) {
+      return prepareTrRequest(message.body);
+    }
+    if (message.body.transaction !== undefined) {
+      if (isTransactionRequest(message.body.transaction)) {
+        return prepareTrRequest(message.body.transaction);
+      }
+    }
+  }
+  throw new Error('Failed extracting transaction details');
+};
 
-exports.handler = (event, context) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
-  awsServerlessExpress.proxy(server, event, context);
+exports.handler = async (event) => {
+  // console.log(`EVENT: ${JSON.stringify(event)}`);
+
+  const transaction = extractTransaction(event.body !== undefined ? JSON.parse(event.body) : event);
+  transaction.id = uuidv4();
+  const result = await docClient.put({
+    TableName: tblName,
+    Item: transaction
+  }).promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      request: JSON.stringify(result)
+    })
+  }
 };
