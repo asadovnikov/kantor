682c6f37051364938cb849792b3626e8b1bb4f33
Oleksandr Kosevych <rn.cruger@gmail.com>
2020-07-02 00:26:18 +0300

    back

--- packages/backoffice-amplify/src/App.js
+++ packages/backoffice-amplify/src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.less';
 import { AdminAppLayout } from './AdminApp';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator, AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
+
+const AuthFlow = ({ children }) => (
+	<AmplifyAuthenticator>
+		<AmplifySignIn headerText='My Custom Sign In Text' slot='sign-in'></AmplifySignIn>
+		{children}
+	</AmplifyAuthenticator>
+);
+
+const AuthFlowApp = () => (
+	<AmplifyAuthenticator>
+		<AmplifySignIn headerText='My Custom Sign In Text' slot='sign-in'></AmplifySignIn>
+	</AmplifyAuthenticator>
+);
 
 function App() {
 	return <AdminAppLayout />;
